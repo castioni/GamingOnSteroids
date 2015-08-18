7f2064fecae7a658ddc12acfba7f34e69181138f
tiritto <tiritto@o2.pl>
2015-08-18 05:22:37 +0200

    Update
    New feature: Placing "safe wards" over the walls!
    Removed: Debug mode available from the settings.

--- wardingAssistant.lua
+++ wardingAssistant.lua
@@ -1,11 +1,11 @@
 local developerMode = false;
 
 WAConfig = scriptConfig("Warding Assistant", "Warding Assistant");
+WAConfig.addParam("Version", "Version 0.3 ALPHA", SCRIPT_PARAM_INFO, false);
 WAConfig.addParam("Enabled", "Script Enabled", SCRIPT_PARAM_ONOFF, true);
 WAConfig.addParam("Nearby", "Only when nearby", SCRIPT_PARAM_ONOFF, true);
 WAConfig.addParam("WardKey", "Put ward on spot", SCRIPT_PARAM_KEYDOWN, string.byte("Z"));
 WAConfig.addParam("AutoWardKey", "Ward closest spot", SCRIPT_PARAM_KEYDOWN, string.byte("O"));
-WAConfig.addParam("Version", "Version 0.2.8 ALPHA", SCRIPT_PARAM_INFO, false);
 class "Point"
   function Point:__init(x, y, z)
     local pos = GetOrigin(x) or type(x) ~= "number" and x or nil
@@ -133,6 +133,113 @@ local wardSpots = {
 	{12103,51,1328} -- Bottom Lane Blue Bush - Bot
 };
 
+local safeWardSpots = {
+
+	--[[{	-- Tribush from Dragon * Not working without mastery...
+		["scoutRequired"]	= false,
+		["heroPosition"]	= {10072,-71.24,3908},
+		["clickPosition"]	= {10297.93,49.03,3358.59},
+		["wardPosition"]	= {10273.9,49.03,3257.76}
+	} --]]
+	
+	{	-- Trisbush from Nashor
+		["clickPosition"]	= {4627,-71,11311},
+		["wardPosition"]	= {4473,51,11457},
+		["heroPosition"]	= {4724,-71,10856}
+	},
+	
+	{	-- Blue Top -> Solo Bush
+		["clickPosition"]	= {3078,54,10868},
+		["wardPosition"]	= {3078,-67,10868},
+		["heroPosition"]	= {2824,54,10356}
+	},
+	
+	{	-- Blue Mid -> round Bush
+		["clickPosition"]	= {5132,51,8373},
+		["wardPosition"]	= {5123,-21,8457},
+		["heroPosition"]	= {5474,51,7906}
+	},
+	
+	{	-- Blue Mid -> River Lane Bush
+		["clickPosition"]	= {6202,51,8132},
+		["wardPosition"]	= {6202,-67,8132},
+		["heroPosition"]	= {5874,51,7656}
+	},
+	
+	{	-- Blue Lizard -> Dragon Pass Bush
+		["clickPosition"]	= {8400,53,4657},
+		["wardPosition"]	= {8523,51,4707},
+		["heroPosition"]	= {8022,53,4258}
+	},
+	
+	{	-- Purple Mid -> Round Bush
+		["clickPosition"]	= {9703,52,6589},
+		["wardPosition"]	= {9823,23,6507},
+		["heroPosition"]	= {9372,52,7008}
+	},
+	
+	{	-- Purple Mid -> River Round Bush
+		["clickPosition"]	= {8705,53,6819},
+		["wardPosition"]	= {8718,95,6764},
+		["heroPosition"]	= {9072,53,7158}
+	},
+	
+	{	-- Purple Bottom -> Solo Bush
+		["clickPosition"]	= {12353,51,4031},
+		["wardPosition"]	= {12023,-66,3757},
+		["heroPosition"]	= {12422,51,4508}
+	},
+	
+	{	-- PPurple Lizard -> Nashor Pass Bush
+		["clickPosition"]	= {6370,56,10359},
+		["wardPosition"]	= {6273,53,10307},
+		["heroPosition"]	= {6824,56,10656}
+	},
+	
+	{	-- Blue Golem -> Blue Lizard
+		["clickPosition"]	= {8163,51,3436},
+		["wardPosition"]	= {8163,51,3436},
+		["heroPosition"]	= {8272,51,2908}
+	},
+	
+	{	-- Red Golem -> Red Lizard
+		["clickPosition"]	= {6678,56,11477},
+		["wardPosition"]	= {6678,53,11477},
+		["heroPosition"]	= {6574,56,12006}
+	},
+	
+	{	-- Blue Top Side Brush
+		["clickPosition"]	= {2302,52,10874},
+		["wardPosition"]	= {2773,-71,11307},
+		["heroPosition"]	= {1774,52,10756}
+	},
+	
+	{	-- Mid Lane Death Brush
+		["clickPosition"]	= {5332,-70,8275},
+		["wardPosition"]	= {5123,-21,8457},
+		["heroPosition"]	= {5874,-70,8306}
+	},
+	
+	{	-- Mid Lane Death Brush Right Side
+		["clickPosition"]	= {9540,71,6657},
+		["wardPosition"]	= {9773,10,6457},
+		["heroPosition"]	= {9022,71,6558}
+	},
+	
+	{	-- Blue Inner Turret Jungle
+		["clickPosition"]	= {6849,50,2252},
+		["wardPosition"]	= {6723,52,2507},
+		["heroPosition"]	= {6874,50,1708}
+	},
+	
+	{	-- Purple Inner Turret Jungle
+		["clickPosition"]	= {8128,52,12658},
+		["wardPosition"]	= {8323,56,12457},
+		["heroPosition"]	= {8122,52,13206}
+	}
+	
+};
+
 --# Warding Sources #--
 local wardItems = {
 	3340,	-- Warding Totem (Trinket)
@@ -154,15 +261,61 @@ function getWardSlot ()
 	return 0;
 end;
 
+--# Checking if player got "Scout" from utility mastery tree #--
+function getScoutStatus ()
+	-- 690 Trinket z masterka 600 bes
+	local itemSlot = GetItemSlot(myHero,3340);
+	if itemSlot == 0 then itemSlot = GetItemSlot(myHero,3361); end;
+	if itemSlot ~= 0 and GetCastRange(itemSlot) == 690
+		then return true;
+	end;
+	return false;
+end;
+
+--# Warding Queue #--
+local wardQueued = nil;
+function checkWardingQueue(wardSlot)
+	
+	-- Don't go any futher if queue is empty!
+	if wardQueued ~= nil then
+	
+		-- Check if the player interrupted the process?
+		if KeyIsDown(0x02)	-- Right Mouse Button
+		or KeyIsDown(0x41)	-- A Key (Attack)
+		or KeyIsDown(0x53)	-- S Key (Stop)
+		then
+			-- Just clear the queue since player doesn't care anymore...
+			wardQueued = nil;
+		end;
+		
+		-- Check if player is already close enough to put this ward...
+		local heroPosition = GetOrigin(myHero);
+		if GetDistance(heroPosition,wardQueued) < 600 then
+		
+			-- Just in case if function will be executed without wardSlot variable...
+			wardSlot = wardSlot or getWardSlot();
+		
+			-- Put ward on it's place already...
+			CastSkillShot(wardSlot,wardQueued);
+			
+			-- Clear the queue since the job is done.
+			wardQueued = nil;
+		end;
+	end;
+end;
+
 -- Let's get this party started...
 OnLoop(function(myHero)
 	
 	-- Is Warding Assistant enabled?
 	if WAConfig.Enabled then
-
+	
 		-- First of all, check if player got any wards with him...
 		local wardSlot = getWardSlot();
 	
+		-- Check if there's any warding process on the run...
+		checkWardingQueue(wardSlot);
+		
 		-- Don't show warding circles if player have no wards.
 		if wardSlot ~= 0 then
 			
@@ -173,8 +326,8 @@ OnLoop(function(myHero)
 			local closestWard = Point(wardSpots[1][1],wardSpots[1][2],wardSpots[1][3]);
 			local closestWardDistance = GetDistanceSqr(closestWard,GetOrigin(myHero));
 			
-			-- Check all warding spots
-			while wardSpots[i] do
+			-- Check all normal warding spots
+			for i=1, #wardSpots, 1 do
 			
 				-- Get the warding spot...
 				local wardPosition = Point(wardSpots[i][1],wardSpots[i][2],wardSpots[i][3]);
@@ -195,8 +348,9 @@ OnLoop(function(myHero)
 					-- Initialize the circle
 					Circle:__init(wardPosition,30);
 					
-					-- Mouseover
-					if Circle:contains(GetMousePos()) then
+					local myszka = GetMousePos();
+					local point = Point(myszka.x,myszka.y,(myszka.z+myszka.y));
+					if Circle:contains(point) then
 						Circle:draw(0xffff0000);
 						
 						-- Manual ward
@@ -208,32 +362,84 @@ OnLoop(function(myHero)
 					else Circle:draw(0xffffffff); end;
 				
 				end;
-				i=i+1; -- Let's move out to next spot...
 			end;
 			
+			-- Check safeWards
+			for i=1, #safeWardSpots, 1 do
 			
-			-- Automatic ward
-			if WAConfig.AutoWardKey then
-				CastSkillShot(wardSlot,closestWard);
+				-- Get the warding spot...
+				local wardPosition = Point(
+					safeWardSpots[i]["wardPosition"][1],
+					safeWardSpots[i]["wardPosition"][2],
+					safeWardSpots[i]["wardPosition"][3]
+				);
+				
+				local heroPosition = Point(
+					safeWardSpots[i]["heroPosition"][1],
+					safeWardSpots[i]["heroPosition"][2],
+					safeWardSpots[i]["heroPosition"][3]
+				);
+				
+				local clickPosition = Point(
+					safeWardSpots[i]["clickPosition"][1],
+					safeWardSpots[i]["clickPosition"][2],
+					safeWardSpots[i]["clickPosition"][3]
+				);
+			
+				-- Initialize the circle
+				Circle:__init(wardPosition,20);
+				Circle:draw(0x66ff0000);
+				
+					
+				-- clickPosition Circle 
+				Circle:__init(heroPosition,60);
+				local myszka = GetMousePos();
+				local point = Point(myszka.x,myszka.y+safeWardSpots[i]["wardPosition"][2],myszka.z);
+				if Circle:contains(GetMousePos()) then
+					Circle:draw(0xff00ff00);
+					
+						local origin = GetOrigin(myHero);
+						tester = GetDistance(origin,clickPosition);
+					-- Manual ward
+					if WAConfig.WardKey then
+						--if IsInDistance(clickPosition,690) then
+						if GetDistance(origin,clickPosition) < 690 then
+							CastSkillShot(wardSlot,clickPosition);
+						else
+							wardQueued = clickPosition;
+							MoveToXYZ(heroPosition); 
+						end;
+					end;
+					
+				-- Just show the circle...
+				else Circle:draw(0xffffffff); end;
+				
+				-- Automatic ward
+				if WAConfig.AutoWardKey then
+					CastSkillShot(wardSlot,closestWard);
+					
+				end;
 			end;
-		end;
 		
-		-- Position scanner. Im using this to get my warding spots...
+		-- Clear pending warding process if there's no wards left (lol)
+		else wardQueued =  nil; end;
+		
+	
+		-- Just some developer tools for you to competely ingore!
 		if developerMode == true then
 			local origin = GetOrigin(myHero);
 			local mousepos = GetMousePos();
 			local myscreenpos = WorldToScreen(1,origin.x,origin.y,origin.z);
-			--DrawText(" " .. math.floor(mousepos.x) .. " " .. math.floor(mousepos.y) .. " " .. math.floor(mousepos.z),24,myscreenpos.x,myscreenpos.y-20,0xff00ff00); 
+			DrawText(" " .. math.floor(mousepos.x) .. " " .. math.floor(mousepos.y) .. " " .. math.floor(mousepos.z),24,myscreenpos.x,myscreenpos.y-20,0xff00ff00); 
 			
-			-- Show ward slots status
 			local text =
-				"Trinket:    " .. GetItemSlot(myHero,3340) .. " " .. CanUseSpell(myHero, GetItemSlot(myHero,3340)) .. "\n" ..
+				"Trinket R:    " .. " " .. GetCastRange(myHero,GetItemSlot(myHero,2044)) .. "\n" ..
 				"TrinkUpgr:  " .. GetItemSlot(myHero,3361) .. " " .. CanUseSpell(myHero, GetItemSlot(myHero,3361)) .. "\n" ..
 				"Sightstone: " .. GetItemSlot(myHero,2049) .. " " .. CanUseSpell(myHero, GetItemSlot(myHero,2049)) .. "\n" ..
 				"Rubystone:  " .. GetItemSlot(myHero,2045) .. " " .. CanUseSpell(myHero, GetItemSlot(myHero,2045)) .. "\n" ..
 				"NormalWard: " .. GetItemSlot(myHero,2044) .. " " .. CanUseSpell(myHero, GetItemSlot(myHero,2044));
 				
-			DrawText(text,24,myscreenpos.x,myscreenpos.y-50,0xff00ff00); 
+			--DrawText(text,24,myscreenpos.x,myscreenpos.y-50,0xff00ff00); 
 		end; 
 	end;
 end)
